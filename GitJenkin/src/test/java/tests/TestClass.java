decbf2c2be31a77ac0b25a0c616f6315b3ec84b6
Priyanka M <Priyanka M@DESKTOP-S3Q7NR0>
2022-08-01 13:10:29 +0530

    Second Commit

--- GitJenkin/src/test/java/tests/TestClass.java
+++ GitJenkin/src/test/java/tests/TestClass.java
@@ -6,19 +6,24 @@ import base.BaseTest;
 
 public class TestClass extends BaseTest {
 	
-	@Test
+	@Test(priority = 1)
 	public void testcase1() {
 		System.out.println("This is testcase1");
 	}
 	
-	@Test
+	@Test(priority = 2)
 	public void testcase2() {
 		System.out.println("This is testcase2");
 	}
 	
-	@Test
+	@Test(priority = 3)
 	public void testcase3() {
 		System.out.println("This is testcase3");
+	}
+	
+	@Test(priority = 4)
+	public void testcase4() {
+		System.out.println("This is testcase4");
 	}
 
 }
