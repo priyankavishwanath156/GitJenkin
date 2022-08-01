d2b161ce29333cd617c677251b013b5cf95d9786
Priyanka M <Priyanka M@DESKTOP-S3Q7NR0>
2022-08-01 12:59:55 +0530

    First Commit

---
+++ GitJenkin/src/test/java/tests/TestClass.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+
+public class TestClass extends BaseTest {
+	
+	@Test
+	public void testcase1() {
+		System.out.println("This is testcase1");
+	}
+	
+	@Test
+	public void testcase2() {
+		System.out.println("This is testcase2");
+	}
+	
+	@Test
+	public void testcase3() {
+		System.out.println("This is testcase3");
+	}
+
+}
